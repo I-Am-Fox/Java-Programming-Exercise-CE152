17406c5ea9e8f35d504d03563dcb9c4e78a8fa0e
Josh <coldire@protonmail.com>
2023-03-27 02:35:18 +0100

    Added getter and setter methods for Tree class, along with varaible names.

--- src/ex2/MainEx1.java
+++ src/ex2/MainEx1.java
@@ -1,4 +1,9 @@
 package ex2;
 
 public class MainEx1 {
+    public static void main(String[] args) {
+
+
+
+    }
 }
--- src/ex2/Tree.java
+++ src/ex2/Tree.java
@@ -1,4 +1,55 @@
 package ex2;
 
 public class Tree {
+    private String location; // location of the tree
+    private String name; // name of the tree
+    private int age; // age of the tree
+    private double height; // height of the tree
+
+    public Tree(String location, String name, int age, double height) {
+        this.location = location;
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    // Getter method for location
+    public String getLocation() {
+        return location;
+    }
+
+    // Setter method for location
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    // Getter method for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter method for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter method for age
+    public int getAge() {
+        return age;
+    }
+
+    // Setter method for age
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Getter method for height
+    public double getHeight() {
+        return height;
+    }
+
+    // Setter method for height
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
